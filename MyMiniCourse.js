36a1977047289697ab5b7783eac5bc2c18d53a60
Psimmons <pjs@cesimmons.com>
2024-04-23 13:32:52 -0400

    Initial MyMiniCourse

---
+++ MyMiniCourse.js
@@ -0,0 +1,10 @@
+// Programmer: Preston Simmons
+// Team Member: LaDanien Shelton
+//Bolt ID:
+//Date: 4.16.2024
+//Program:MyMiniCourse
+//Program URL: https://edu.sphero.com/program/16743350/edit
+
+async function startProgram() {
+	// Write code here
+}
