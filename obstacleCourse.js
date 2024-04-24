257ee4c3f52548ccd5969f3548844ccb4a9e4cc2
Psimmons <pjs@cesimmons.com>
2024-04-24 14:20:40 -0400

    Check Point 2

--- obstacleCourse.js
+++ obstacleCourse.js
@@ -1,16 +1,19 @@
-// Programmer: Preston Simmons
-// Team Member: LaDanien Shelton
-//Bolt ID:6B78
-//Date: 4.16.2024
-//Program:Obstacle Course
-//Program URL: https://edu.sphero.com/program/16752737/edit
-//Partner 1
+// Programer: LaDanien Shelton
+// Team Member: Prestion Simmons
+// Blot ID:6B78
+// Date:4.16.2024
+// Program: Sphero Obstical Course #1 
+// Progarm URL: https://edu.sphero.com/program/16700413/ed
 
 //Check Point 1
 //Programmer 1: Preston Simmons
 async function startProgram() {
 	await roll((getHeading() + 0), 60, 1.55);
 	await delay(1);
+	//Check point 2
+    //Programmer 2: laDanien Shelton
+	await roll((getHeading() + 90), 60, 1.3);
+    await delay(1);
 	
 	
 	
