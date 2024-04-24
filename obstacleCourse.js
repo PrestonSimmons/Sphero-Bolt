9945b8dbf1089088310c8bc46762b31a713cf605
Psimmons <pjs@cesimmons.com>
2024-04-24 14:02:23 -0400

    Check Point 1

--- obstacleCourse.js
+++ obstacleCourse.js
@@ -2,5 +2,14 @@
 // Team Member: LaDanien Shelton
 //Bolt ID:6B78
 //Date: 4.16.2024
-//Program:MyMiniCourse
-//Program URL: https://edu.sphero.com/program/16743350/edit
+//Program:Obstacle Course
+//Program URL: https://edu.sphero.com/program/16752737/edit
+//Partner 1
+
+async function startProgram() {
+	await roll((getHeading() + 0), 60, 1.55);
+	await delay(1);
+	
+	
+	
+}
