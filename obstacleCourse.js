be676fdb703ee5ad9aa4781d4aa3eaa9562f248a
Psimmons <pjs@cesimmons.com>
2024-04-26 14:01:18 -0400

    // Preston Simmons - PC #16 - Checkpoint two - blue + Sound

--- obstacleCourse.js
+++ obstacleCourse.js
@@ -1,32 +1,32 @@
-// Programer: LaDanien Shelton
-// Team Member: Prestion Simmons
-// Blot ID:6B78
-// Date:4.16.2024
-// Program: Sphero Obstical Course #1 
-// Progarm URL: https://edu.sphero.com/program/16700413/ed
-
-//Check Point 1
-//Programmer 1: Preston Simmons
-async function startProgram() {
-	await roll((getHeading() + 0), 60, 1.55);
-	await delay(1);
-//Check point 2
-//Programmer 2: laDanien Shelton
-	await roll((getHeading() + 90), 60, 1.3);
-    await delay(1);
-
-
-
-}
-
- // ---------------------------------------------------------------
+// Programmer: Preston Simmons
+// Team Member: LaDanien Shelton
+//Bolt ID:6B78
+//Date: 4.16.2024
+//Program:Obstacle Course
+//Program URL: https://edu.sphero.com/program/16752737/edit
+//Partner 1
 
 // New Maze #2  
 
 // LaDanien Shelton - PC # 15 - Checkpoint one - Red 
+/*
 async function startProgram() {
 	setMainLed({ r: 225, g: 0, b: 0 });
 	await roll((getHeading() + 0), 60, 2.7);
 	await delay(1);
 	
 } 
+*/
+
+
+// Preston Simmons - PC #16 - Checkpoint two - blue + Sound
+async function startProgram() {
+	setMainLed({ r: 225, g: 0, b: 0 });
+	await roll((getHeading() + 0), 60, 2.3);
+	await delay(1);
+	await Sound.Menu.Accept.play(true);
+	setMainLed({ r: 0, g: 0, b: 225 });
+	await roll((getHeading() + 90), 60, 1.3);
+	await delay(1);
+} 
+
