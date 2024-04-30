4f5a3a22d879a0f983b278d52e22f4c8bcc4de20
Psimmons <pjs@cesimmons.com>
2024-04-30 13:29:45 -0400

    Adding Information

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+// Programmer: Preston Simmons
+// Team Member: LaDanien Shelton
+//Bolt ID: 6B78
+//Date: 4.30.2024
+//Program: Maze Competition
+//Program URL:
+
+async function startProgram() {
+	// Write code here
+}
